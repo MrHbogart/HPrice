a9083a03079b8935b20d1ca292c7b2f9044eea3c
Hossein Saeidi <ho.saeidi77@gmail.com>
2021-07-22 23:06:16 +0430

    change simple cashdata(actually HPrice) to something I named it HPrice         """ what are we doing here?         like cash data in other repository, we make neowave cashdata form OHLC data, but in cash data, frames are static and we forced to find         a high and a low price in a frame(e.g. n=4 candles) but here we shift our data and make cashdata n time and after we finished         (e.g. we make n=4 complete cash data) we compare all cash data 's and store those are stayed in all cash data 's         after that we have to remove those points that are not extremom """

--- HPrice/HPrice.py
+++ HPrice/HPrice.py
@@ -8,36 +8,35 @@ class HPrice:
         self.n = n
 
     def HPrice(self):
-        date = []
-        price = []
-        volume = []
+        """ what are we doing here?
+        like cash data in other repository, we make neowave cashdata form OHLC data, but in cash data, frames are static and we forced to find
+        a high and a low price in a frame(e.g. n=4 candles) but here we shift our data and make cashdata n time and after we finished
+        (e.g. we make n=4 complete cash data) we compare all cash data 's and store those are stayed in all cash data 's
+        after that we have to remove those points that are not extremom """
 
-        for i in range(self.data.shape[0] % self.n, self.data.shape[0], self.n):
-            sub_df = self.data[i: i+self.n]
-            max_index = sub_df['high'].idxmax()
-            min_index = sub_df['low'].idxmin()
-            if max_index < min_index:
-                date.append(sub_df['date'][max_index])
-                date.append(sub_df['date'][min_index])
+        date_list = []
+        price_list = []
+        volume_list = []
+        for j in range(self.n):
+            date = []
+            price = []
+            volume = []
 
-                price.append(sub_df['high'][max_index])
-                price.append(sub_df['low'][min_index])
+            for i in range(self.data.shape[0] % self.n +j, self.data.shape[0], self.n):
+                sub_df = self.data[i: i+self.n]
+                max_index = sub_df['high'].idxmax()
+                min_index = sub_df['low'].idxmin()
+                if max_index < min_index:
+                    date.append(sub_df['date'][max_index])
+                    date.append(sub_df['date'][min_index])
 
-                volume.append(sub_df['volume'][max_index])
-                volume.append(sub_df['volume'][min_index])
+                    price.append(sub_df['high'][max_index])
+                    price.append(sub_df['low'][min_index])
 
-            elif max_index > min_index:
-                date.append(sub_df['date'][min_index])
-                date.append(sub_df['date'][max_index])
+                    volume.append(sub_df['volume'][max_index])
+                    volume.append(sub_df['volume'][min_index])
 
-                price.append(sub_df['low'][min_index])
-                price.append(sub_df['high'][max_index])
-
-                volume.append(sub_df['volume'][min_index])
-                volume.append(sub_df['volume'][max_index])
-
-            else:
-                if sub_df['close'][max_index] > sub_df['open'][max_index]:
+                elif max_index > min_index:
                     date.append(sub_df['date'][min_index])
                     date.append(sub_df['date'][max_index])
 
@@ -48,15 +47,41 @@ class HPrice:
                     volume.append(sub_df['volume'][max_index])
 
                 else:
-                    date.append(sub_df['date'][max_index])
-                    date.append(sub_df['date'][min_index])
+                    if sub_df['close'][max_index] > sub_df['open'][max_index]:
+                        date.append(sub_df['date'][min_index])
+                        date.append(sub_df['date'][max_index])
 
-                    price.append(sub_df['high'][max_index])
-                    price.append(sub_df['low'][min_index])
+                        price.append(sub_df['low'][min_index])
+                        price.append(sub_df['high'][max_index])
 
-                    volume.append(sub_df['volume'][max_index])
-                    volume.append(sub_df['volume'][min_index])
+                        volume.append(sub_df['volume'][min_index])
+                        volume.append(sub_df['volume'][max_index])
 
+                    else:
+                        date.append(sub_df['date'][max_index])
+                        date.append(sub_df['date'][min_index])
 
-            result = pd.DataFrame(list(zip(date, price, volume)), columns=['date', 'price', 'volume'])
+                        price.append(sub_df['high'][max_index])
+                        price.append(sub_df['low'][min_index])
+
+                        volume.append(sub_df['volume'][max_index])
+                        volume.append(sub_df['volume'][min_index])
+
+            date_list.append(date)
+            price_list.append(price)
+            volume_list.append(volume)
+        H_date = []
+        H_price = []
+        H_volume = []
+        for i in range(len(date_list[0])):
+            is_there = True
+            for j in date_list:
+                if date_list[0][i] not in j:
+                    is_there = False
+            if is_there:
+                H_date.append(date_list[0][i])
+                H_price.append(price_list[0][i])
+                H_volume.append(volume_list[0][i])
+
+        result = pd.DataFrame(list(zip(H_date, H_price, H_volume)), columns=['date', 'price', 'volume'])
         return result
