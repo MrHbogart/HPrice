7a81654585b2b7a6e5fc000958f40569228ef8b2
Hossein Saeidi <ho.saeidi77@gmail.com>
2021-07-23 04:54:53 +0430

    commented plotly and make setup.py file to make package installable

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/MrHbogart/HPrice",
     classifiers=[
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
